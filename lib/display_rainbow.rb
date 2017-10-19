f31e7e1bfe091b6b1cae4e5724693516ccc8405b
Blessing Ebowe <blessingebowe@gmail.com>
2017-10-19 10:24:23 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,8 +3,10 @@ def display_rainbow(colors)
   str = ""
   for i in colors
     if (i === colors.length - 1)
-      return str += "#{i.chr.upcase}: #{i}"
+      str += "#{i.chr.upcase}: #{i}"
+    else
+      str += "#{i.chr.upcase}: #{i}, "
     end
-    str += "#{i.chr.upcase}: #{i}, "
   end
+  puts str
 end
