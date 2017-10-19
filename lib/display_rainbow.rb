0cc5522a4bf4bb2c3cdf86383bd0d8802afbd4e3
Blessing Ebowe <blessingebowe@gmail.com>
2017-10-19 10:32:09 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,9 +3,9 @@ def display_rainbow(colors)
   str = ""
   colors.each_with_index do |val, ind|
     if (ind === (colors.length - 1))
-      str += "#{i.chr.upcase}: #{i}"
+      str += "#{val.chr.upcase}: #{val}"
     else
-      str += "#{i.chr.upcase}: #{i}, "
+      str += "#{val.chr.upcase}: #{val}, "
     end
   end
   puts str
